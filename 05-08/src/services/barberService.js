0fac16afa7c17d2e6ed2a13a78e6c86d79212827
sarah! <sarah.c.nascimento10@gmail.com>
2026-08-05 17:13:38 -0300

    terminando mais uma parte

--- 05-08/src/services/barberService.js
+++ 05-08/src/services/barberService.js
@@ -1,1 +1,31 @@
 import {barberRepository} from '../repositories/barberRepository.js'
+
+export const BarbeiroService = {
+    async getALLbarbeiros(){
+        return await barberRepository.findAll()
+    },
+
+    async getbarbeiro (id) {
+        const barbeiroExistente = await barberRepository.findById(id)
+        if(!barbeiroExistente) throw new Error ("barbeiro nao foi encontrado")
+        return barbeiroExistente
+    },
+
+    async createBarbeiro (barbeiroRequisicao){
+        if(barbeiroRequisicao.nome.length < 3) {
+            throw new Error ("nome do barbeiro deve ter no minimo 3 caracteres")
+        }
+        return await barberRepository.create(barbeiroRequisicao);
+    },
+
+    
+
+
+
+
+
+
+
+
+
+}
