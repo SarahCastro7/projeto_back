1a835520659fa35b6ab76f454bd3845813f715ba
sarah! <sarah.c.nascimento10@gmail.com>
2026-08-12 17:15:42 -0300

    12/08
    precisa arrumar a ultima parte, mas de resto a maioria esta certa

--- 12-08/backend/server.js
+++ 12-08/backend/server.js
@@ -1,7 +1,51 @@
+const express = require('express');
+const {Pool} = require('pg');
+const bcrypt = require('pg');
+const cors = require('cors');
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+const port = 3000;
 
-const user = new User(nome, email, senha);
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'NewUsuarios',
+    password: 'senai',
+    port: 5432
+});
 
-console.log(user.nome);
-console.log(user.email);
-console.log(user.senha);
+//aqui fica a rota de login
+
+app.post('/usuarios', async (req, res) =>{
+    const {uname, psw} = req.body; 
+
+    if (!uname || !psw) {
+        return res.status(400).json({ error: 'Nome de usuário e senha são obrigatórios' });
+    }
+
+    try {
+        const result =await pool.query(
+            'SELECT * FROM usuarios WHERE uname = $1', 
+            [uname]);
+       if (result.rows.length === 0) {
+            return res.status(401).json({ error: 'usuario não encontrado' });
+        }
+        const usuario = result.rows[0];
+        //p comparar a senha
+        const senhaValid = await bcrypt.compare(psw, usuario.psw);
+        if (!senhaValid) {
+            return res.status(401).json({ error: 'senha errada!' });
+        }
+
+}
+
+//login esta ok TEM QUE CONCERTAR AQ
+return res.json({sucess: true, message: 'seja bem-vindo!'}, username: uname );
+})
+}catch (error) { 'erro ao fazer o login', error.message;
+    res.status(500).json({ sucess: false, message: 'erro ao fazer o login', error.message });
+}
+
+const PORT = 3000,
